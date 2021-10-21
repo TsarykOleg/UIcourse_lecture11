4066460d19de0021aa9c87d116773c224737ebe2
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-21 23:46:39 +0300

    added

---
+++ multiply.js
@@ -0,0 +1,9 @@
+// Multiply a given number by eight if it is an even number and by nine otherwise //   
+let test = prompt('Enter the number: ');
+if (test % 2 == 0) {alert (test*8)} 
+else {alert(test*9)
+    }
+
+ 
+
+
